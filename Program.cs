c9304111fcd8d4c90118f6f6898f4806c9ad12d0
Eloá <antuneseloa23@gmail.com>
2025-09-19 22:29:45 -0300

    exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Peruibe");
+Console.WriteLine("Italhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Sâo Vicente");
+Console.WriteLine("Santos");
+
+
+
+
+
+
+
+
+
+
